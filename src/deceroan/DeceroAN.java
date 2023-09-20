5922b99208b8ffb5ac237a63dfe0c65435e94c1d
Migue <Migue@DESKTOP-GVDJK75>
2023-09-19 19:30:26 -0600

    le agregue otro codigo

--- src/deceroan/DeceroAN.java
+++ src/deceroan/DeceroAN.java
@@ -9,13 +9,24 @@ public class DeceroAN {
 
 
     public static void main(String[] args) {
-        int num,contador;
+        int num,contador,acumulador=0;
         
-        num = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero "));
+        //de cero a N 
+        /*num = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero "));
         
         for (contador=1;contador<=num;contador++){
             System.out.println(+contador);
-        }
+        }*/
+        
+        // pedir 5 numeros y mostrara la suma total
+        
+       for (int i =1; i <=5;i++){
+           num = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"));
+           acumulador+=num;
+       }  
+        JOptionPane.showMessageDialog(null, "la suma total de esos numeros es: "+acumulador);
+        
+        
     }
     
 }
